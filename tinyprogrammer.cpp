90a3d15f64f685112489ca0fa6173c5e533da8dc
developer <you@example.com>
2021-06-12 07:44:40 +0200

    licencing

--- tinyprogrammer.cpp
+++ tinyprogrammer.cpp
@@ -1,3 +1,17 @@
+    # This file is part of tinyavrprogrammer.
+
+    # tinyavrprogrammer is free software: you can redistribute it and/or modify
+    # it under the terms of the GNU General Public License as published by
+    # the Free Software Foundation, version 3.
+
+    # tinyavrprogrammer is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    # GNU General Public License for more details.
+
+    # You should have received a copy of the GNU General Public License
+    # along with tinyavrprogrammer.  If not, see <https://www.gnu.org/licenses/>.
+
 #include "hmain.hpp"
 int main()
 {
